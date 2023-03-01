6ffb24a1ebd4c529c9068e58f4dd2d230faa30e4
yupeng <yupeng@anpro-tech.com>
2023-03-01 20:12:33 +0800

    2373.矩阵中的局部最大值

---
+++ 2373.矩阵中的局部最大值.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=2373 lang=golang
+ *
+ * [2373] 矩阵中的局部最大值
+ */
+
+// @lc code=start
+func largestLocal(grid [][]int) [][]int {
+	n := len(grid)
+	ans := make([][]int, n-2)
+	for i := 1; i <= len(ans); i++ {
+		row := make([]int, n-2)
+		for j := 1; j <= len(row); j++ {
+			mx := grid[i][j]
+			for r := i - 1; r <= i+1; r++ {
+				for c := j - 1; c <= j+1; c++ {
+					if grid[r][c] > mx {
+						mx = grid[r][c]
+					}
+				}
+			}
+			row[j-1] = mx
+		}
+		ans[i-1] = row
+	}
+	return ans
+}
+
+// @lc code=end
