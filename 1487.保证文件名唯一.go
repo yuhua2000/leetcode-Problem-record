e390e4652be6459190c600fd041e6daf8fce913a
yupeng <yupeng@anpro-tech.com>
2023-03-06 20:06:24 +0800

    1653.使字符串平衡的最少删除次数

---
+++ 1487.保证文件名唯一.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"strconv"
+)
+
+/*
+ * @lc app=leetcode.cn id=1487 lang=golang
+ *
+ * [1487] 保证文件名唯一
+ */
+
+// @lc code=start
+func getFolderNames(names []string) []string {
+	ans := make([]string, len(names))
+	index := map[string]int{}
+	for p, name := range names {
+		i := index[name]
+		if i == 0 {
+			index[name] = 1
+			ans[p] = name
+			continue
+		}
+		for index[name+"("+strconv.Itoa(i)+")"] > 0 {
+			i++
+		}
+		t := name + "(" + strconv.Itoa(i) + ")"
+		ans[p] = t
+		index[t] = 1
+		index[name] = i + 1
+	}
+	return ans
+}
+
+// @lc code=end
---
+++ 1653.使字符串平衡的最少删除次数.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import "math"
+
+/*
+ * @lc app=leetcode.cn id=1653 lang=golang
+ *
+ * [1653] 使字符串平衡的最少删除次数
+ */
+
+// @lc code=start
+func minimumDeletions(s string) int {
+	n := len(s)
+	ans := make([][2]int, n)
+	a, b := 0, 0
+	for i, c := range s {
+		if c == 'b' {
+			b++
+		}
+		ans[i][1] = b
+		if s[n-1-i] == 'a' {
+			a++
+		}
+		ans[n-1-i][0] = a
+	}
+	min := math.MaxInt
+	for _, a := range ans {
+		if a[0]+a[1]-1 < min {
+			min = a[0] + a[1] - 1
+		}
+	}
+	return min
+}
+
+// @lc code=end
