4fb82882a1710b9a38fba3a1d80d7c5c2cd17232
yupeng <yupeng@anpro-tech.com>
2023-03-13 20:36:19 +0800

    2383.赢得比赛需要的最少训练时长

---
+++ 2383.赢得比赛需要的最少训练时长.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=2383 lang=golang
+ *
+ * [2383] 赢得比赛需要的最少训练时长
+ */
+
+// @lc code=start
+func minNumberOfHours(initialEnergy int, initialExperience int, energy []int, experience []int) int {
+	ans := 0
+	n := len(energy)
+	for i := 0; i < n; {
+		if initialEnergy > energy[i] && initialExperience > experience[i] {
+			initialEnergy -= energy[i]
+			initialExperience += experience[i]
+			i++
+		} else if initialEnergy <= energy[i] {
+			temp := energy[i] - initialEnergy + 1
+			ans += temp
+			initialEnergy += temp
+		} else if initialExperience <= experience[i] {
+			temp := experience[i] - initialExperience + 1
+			ans += temp
+			initialExperience += temp
+		}
+	}
+	return ans
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
+
