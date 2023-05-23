535b795a8c82dc1158f774199799f7cc4e526ca8
yupeng <yupeng@anpro-tech.com>
2023-05-23 20:14:55 +0800

    1090. 受标签影响的最大值

---
+++ 1090.受标签影响的最大值.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1090 lang=golang
+ *
+ * [1090] 受标签影响的最大值
+ */
+
+// @lc code=start
+func largestValsFromLabels(values []int, labels []int, numWanted int, useLimit int) int {
+	n := len(values)
+	idx := make([]int, n)
+	for i := range idx {
+		idx[i] = i
+	}
+	sort.Slice(idx, func(i, j int) bool {
+		return values[idx[i]] > values[idx[j]]
+	})
+	ans, choose := 0, 0
+	cnt := make(map[int]int)
+	for i := range idx {
+		label := labels[idx[i]]
+		if cnt[label] >= useLimit {
+			continue
+		}
+		choose++
+		ans += values[idx[i]]
+		cnt[label]++
+		if choose == numWanted {
+			break
+		}
+	}
+	return ans
+}
+
+// @lc code=end
--- 2446.判断两个事件是否存在冲突.go
+++ 2446.判断两个事件是否存在冲突.go
@@ -6,7 +6,12 @@
 
 // @lc code=start
 func haveConflict(event1 []string, event2 []string) bool {
-
+	return !(event2[1] < event1[0] || event2[0] > event1[1])
+	startTime11 := (int(event1[0][0]-'0')*10+int(event1[0][1]-'0'))*60 + (int(event1[0][3]-'0')*10 + int(event1[0][4]-'0'))
+	startTime12 := (int(event1[1][0]-'0')*10+int(event1[1][1]-'0'))*60 + (int(event1[1][3]-'0')*10 + int(event1[1][4]-'0'))
+	startTime21 := (int(event2[0][0]-'0')*10+int(event2[0][1]-'0'))*60 + (int(event2[0][3]-'0')*10 + int(event2[0][4]-'0'))
+	startTime22 := (int(event2[1][0]-'0')*10+int(event2[1][1]-'0'))*60 + (int(event2[1][3]-'0')*10 + int(event2[1][4]-'0'))
+	return !(startTime22 < startTime11 || startTime21 > startTime12)
 }
-// @lc code=end
 
+// @lc code=end
