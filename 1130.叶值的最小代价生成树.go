4c11b6dd057fa5b9e51f5e1b7120431dbd9494ed
yupeng <yupeng@anpro-tech.com>
2023-05-31 20:33:23 +0800

    1130.叶值的最小代价生成树

---
+++ 1130.叶值的最小代价生成树.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1130 lang=golang
+ *
+ * [1130] 叶值的最小代价生成树
+ */
+
+// @lc code=start
+func mctFromLeafValues(arr []int) int {
+	res, stk := 0, []int{}
+	for _, x := range arr {
+		for len(stk) > 0 && stk[len(stk)-1] <= x {
+			if len(stk) == 1 || stk[len(stk)-2] > x {
+				res += stk[len(stk)-1] * x
+			} else {
+				res += stk[len(stk)-1] * stk[len(stk)-2]
+			}
+			stk = stk[:len(stk)-1]
+		}
+		stk = append(stk, x)
+	}
+	for len(stk) > 1 {
+		res += stk[len(stk)-1] * stk[len(stk)-2]
+		stk = stk[:len(stk)-1]
+	}
+	return res
+}
+
+// @lc code=end
