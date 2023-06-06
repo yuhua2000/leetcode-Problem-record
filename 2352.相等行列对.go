24f2874fcde9452ffd9c94dbf6f3dc66885b1a12
yuhua <2651034096@qq.com>
2023-06-06 09:32:15 +0800

    2352.相等行列对

---
+++ 2352.相等行列对.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2352 lang=golang
+ *
+ * [2352] 相等行列对
+ */
+
+// @lc code=start
+func equalPairs(grid [][]int) int {
+	n := len(grid)
+	cnt := make(map[string]int)
+	for i := 0; i < n; i++ {
+		cnt[fmt.Sprint(grid[i])]++
+	}
+	res := 0
+	for i := 0; i < n; i++ {
+		var arr = make([]int, 0, n)
+		for j := 0; j < n; j++ {
+			arr = append(arr, grid[j][i])
+		}
+		res += cnt[fmt.Sprint(arr)]
+	}
+	return res
+}
+
+// @lc code=end
