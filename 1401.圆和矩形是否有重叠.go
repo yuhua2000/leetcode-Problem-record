0de4f6c128d927437658e53ecd5d2e0e43a68c58
yuhua <2651034096@qq.com>
2023-06-25 20:07:23 +0800

    1401.圆和矩形是否有重叠

---
+++ 1401.圆和矩形是否有重叠.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1401 lang=golang
+ *
+ * [1401] 圆和矩形是否有重叠
+ */
+
+// @lc code=start
+func checkOverlap(radius int, xCenter int, yCenter int, x1 int, y1 int, x2 int, y2 int) bool {
+	var dist int
+	if xCenter < x1 || xCenter > x2 {
+		dist += min((xCenter-x1)*(xCenter-x1), (xCenter-x2)*(xCenter-x2))
+	}
+	if yCenter < y1 || yCenter > y2 {
+		dist += min((yCenter-y1)*(yCenter-y1), (yCenter-y2)*(yCenter-y2))
+	}
+	return dist <= radius*radius
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
