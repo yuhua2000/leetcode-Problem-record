1877424ad4f9ad9f2d9b9ec780117095a7ca742a
yuhua <2651034096@qq.com>
2023-06-30 09:34:49 +0800

    2490.回环句

---
+++ 2490.回环句.c
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=2490 lang=c
+ *
+ * [2490] 回环句
+ */
+
+// @lc code=start
+bool isCircularSentence(char *sentence)
+{
+    int n = strlen(sentence);
+    for (int i = 0; i < n; i++)
+    {
+        if (sentence[i] == ' ')
+        {
+            if (sentence[i - 1] != sentence[i + 1])
+            {
+                return false;
+            }
+        }
+    }
+    return sentence[0] == sentence[n - 1];
+}
+// @lc code=end
