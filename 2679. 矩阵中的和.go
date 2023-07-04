e8bafae92edc629b926976835c21a6789c5a75c7
yuhua <2651034096@qq.com>
2023-07-04 09:25:59 +0800

    2679.矩阵中的和

---
+++ 2679. 矩阵中的和.go
@@ -0,0 +1,13 @@
+func matrixSum(nums [][]int) (result int) {
+	ans := make([]int, len(nums[0]))
+	for i := 0; i < len(nums); i++ {
+		sort.Ints(nums[i])
+		for j := 0; j < len(nums[i]); j++ {
+			if nums[i][j] > ans[j] {
+				result += (nums[i][j] - ans[j])
+				ans[j] = nums[i][j]
+			}
+		}
+	}
+	return result
+}
