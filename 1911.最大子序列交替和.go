96b1987604d56a6d2adfb895973a62d5256cb6fb
yuhua <2651034096@qq.com>
2023-07-11 09:17:07 +0800

    1911.最大子序列交替和

---
+++ 1911.最大子序列交替和.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1911 lang=golang
+ *
+ * [1911] 最大子序列交替和
+ */
+
+// @lc code=start
+func maxAlternatingSum(nums []int) int64 {
+	even, odd := nums[0], 0
+	for i := 0; i < len(nums); i++ {
+		even = max(even, odd+nums[i])
+		odd = max(odd, even-nums[i])
+	}
+	return int64(even)
+
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
