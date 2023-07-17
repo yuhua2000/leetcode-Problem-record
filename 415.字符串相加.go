da60bba5bcad658eb16d71c003076b44acf4e1ac
yuhua <2651034096@qq.com>
2023-07-17 10:44:14 +0800

    415.字符串相加

---
+++ 415.字符串相加.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=415 lang=golang
+ *
+ * [415] 字符串相加
+ */
+
+// @lc code=start
+func addStrings(num1 string, num2 string) string {
+	result := ""
+	var sum byte = 0
+	for i, j := len(num1)-1, len(num2)-1; i >= 0 || j >= 0 || sum != 0; {
+		if i >= 0 {
+			sum += num1[i] - '0'
+		}
+		if j >= 0 {
+			sum += num2[j] - '0'
+		}
+		result = string(sum%10+'0') + result
+		sum = sum / 10
+		i--
+		j--
+	}
+	return result
+}
+
+// @lc code=end
