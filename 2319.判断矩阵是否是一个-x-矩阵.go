6c9fdaa6b5bb1fe84a63959fd440c2d1acfd8f01
yuhua <2651034096@qq.com>
2023-07-24 11:14:16 +0800

    2319.判断矩阵是否是一个-x-矩阵

---
+++ 2319.判断矩阵是否是一个-x-矩阵.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2319 lang=golang
+ *
+ * [2319] 判断矩阵是否是一个 X 矩阵
+ */
+
+// @lc code=start
+func checkXMatrix(grid [][]int) bool {
+	n, m := len(grid), len(grid[0])
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if j == i || j == n-i-1 {
+				if grid[i][j] == 0 {
+					return false
+				}
+			} else if grid[i][j] != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// @lc code=end
