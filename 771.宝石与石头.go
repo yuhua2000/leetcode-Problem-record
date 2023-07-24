f0c9c7d35107b6ce61990d6cd60f10b93c692a1a
yuhua <2651034096@qq.com>
2023-07-24 09:15:52 +0800

    877.宝石与石头

---
+++ 771.宝石与石头.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=771 lang=golang
+ *
+ * [771] 宝石与石头
+ */
+
+// @lc code=start
+func numJewelsInStones(jewels string, stones string) (result int) {
+	jewelMap := make(map[byte]bool, len(jewels))
+	for i := range jewels {
+		jewelMap[jewels[i]] = true
+	}
+
+	for i := range stones {
+		if jewelMap[stones[i]] {
+			result++
+		}
+	}
+
+	return
+}
+// @lc code=end
