bdf36adec6eeba69a29181d8fc170d2a173e3854
yuhua <2651034096@qq.com>
2023-07-25 09:23:26 +0800

    2208.将数组和减半的最少操作次数

---
+++ 2208.将数组和减半的最少操作次数.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=2208 lang=golang
+ *
+ * [2208] 将数组和减半的最少操作次数
+ */
+
+// @lc code=start
+
+type PriorityQueue []float64
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i] > pq[j]
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	*pq = append(*pq, x.(float64))
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old, n := *pq, len(*pq)
+	x := old[n-1]
+	*pq = old[0 : n-1]
+	return x
+}
+
+func halveArray(nums []int) (res int) {
+	sum := 0
+	pq := &PriorityQueue{}
+	for _, n := range nums {
+		heap.Push(pq, float64(n))
+		sum += n
+	}
+	half := float64(sum) / 2
+	for half > 0 {
+		x := heap.Pop(pq).(float64) / 2
+		half -= x
+		res++
+		heap.Push(pq, x)
+	}
+	return res
+}
+
+// @lc code=end
