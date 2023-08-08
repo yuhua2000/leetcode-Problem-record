ebfa1e2dc81160131c5e268fcbe0887fa79f5bae
yuhua <2651034096@qq.com>
2023-08-08 09:06:58 +0800

    344.反转字符串

---
+++ 344.反转字符串.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=golang
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+func reverseString(s []byte) {
+	for l, r := 0, len(s)-1; l < r; l++ {
+		s[l], s[r] = s[r], s[l]
+		r--
+	}
+}
+
+// @lc code=end
