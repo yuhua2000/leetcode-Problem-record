ca66688938d31a403de253849f8f3f6c5f5554b5
yuhua <2651034096@qq.com>
2023-08-08 09:33:16 +0800

    1749.任意子数组和的绝对值的最大值

---
+++ 1749.任意子数组和的绝对值的最大值.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=1749 lang=golang
+ *
+ * [1749] 任意子数组和的绝对值的最大值
+ */
+
+// @lc code=start
+func maxAbsoluteSum(nums []int) int {
+	positiveMax, negativeMin := 0, 0
+	positiveSum, negativeSum := 0, 0
+	for _, num := range nums {
+		positiveSum += num
+		positiveMax = max(positiveMax, positiveSum)
+		positiveSum = max(0, positiveSum)
+
+		negativeSum += num
+		negativeMin = min(negativeMin, negativeSum)
+		negativeSum = min(0, negativeSum)
+	}
+	return max(positiveMax, -negativeMin)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
