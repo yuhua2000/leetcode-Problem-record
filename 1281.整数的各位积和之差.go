d52c88c25a665279e6cb5136d6b7575a943d8e45
yuhua <2651034096@qq.com>
2023-08-09 09:15:03 +0800

    1282.整数的各位积和之差

---
+++ 1281.整数的各位积和之差.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1281 lang=golang
+ *
+ * [1281] 整数的各位积和之差
+ */
+
+// @lc code=start
+func subtractProductAndSum(n int) int {
+	m, s := 1, 0
+	for n > 0 {
+		x := n % 10
+		n = n / 10
+		m *= x
+		s += x
+	}
+	return m - s
+}
+
+// @lc code=end
