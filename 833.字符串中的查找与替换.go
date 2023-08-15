e4a5b1d6932d8ac4061b9731f8f52cab96adf18b
yuhua <2651034096@qq.com>
2023-08-15 16:17:25 +0800

    833.字符串中的查找与替换

---
+++ 833.字符串中的查找与替换.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=833 lang=golang
+ *
+ * [833] 字符串中的查找与替换
+ */
+
+// @lc code=start
+func findReplaceString(s string, indices []int, sources []string, targets []string) string {
+	n, m := len(s), len(indices)
+	ops := make([]int, len(indices))
+	for i := range ops {
+		ops[i] = i
+	}
+	sort.Slice(ops, func(i, j int) bool {
+		return indices[ops[i]] < indices[ops[j]]
+	})
+
+	ans := strings.Builder{}
+	pt := 0
+	for i := 0; i < n; {
+		for pt < m && indices[ops[pt]] < i {
+			pt++
+		}
+		succeed := false
+		for pt < m && indices[ops[pt]] == i {
+			if s[i:i+min(len(sources[ops[pt]]), n-i)] == sources[ops[pt]] {
+				succeed = true
+				break
+			}
+			pt++
+		}
+		if succeed {
+			ans.WriteString(targets[ops[pt]])
+			i += len(sources[ops[pt]])
+		} else {
+			ans.WriteByte(s[i])
+			i++
+		}
+	}
+	return ans.String()
+
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
