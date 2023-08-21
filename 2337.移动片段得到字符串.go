68d6eba29ac2be9241920140c2165d70d28f224b
yuhua <2651034096@qq.com>
2023-08-21 17:49:31 +0800

    2337.移动片段得到字符串

---
+++ 2337.移动片段得到字符串.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=2337 lang=golang
+ *
+ * [2337] 移动片段得到字符串
+ */
+
+// @lc code=start
+func canChange(start string, target string) bool {
+	i, j, n := 0, 0, len(start)
+	for i < n && j < n {
+		for i < n && start[i] == '_' {
+			i++
+		}
+		for j < n && target[j] == '_' {
+			j++
+		}
+		if i < n && j < n {
+			if start[i] != target[j] {
+				return false
+			}
+			c := start[i]
+			if c == 'L' && i < j || c == 'R' && i > j {
+				return false
+			}
+			i++
+			j++
+		}
+	}
+	for i < n {
+		if start[i] != '_' {
+			return false
+		}
+		i++
+	}
+	for j < n {
+		if target[j] != '_' {
+			return false
+		}
+		j++
+	}
+	return true
+}
+
+// @lc code=end
