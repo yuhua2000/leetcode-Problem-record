8db39f1dd9d4a88ecc95dd2ee103cbe5b2a052c3
yuhua <2651034096@qq.com>
2023-08-24 09:18:05 +0800

    1267.统计参与通信的服务器

---
+++ 1267.统计参与通信的服务器.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1267 lang=golang
+ *
+ * [1267] 统计参与通信的服务器
+ */
+
+// @lc code=start
+func countServers(grid [][]int) int {
+	row := make([]int, len(grid))
+	column := make([]int, len(grid[0]))
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 {
+				row[i]++
+				column[j]++
+			}
+		}
+	}
+
+	result := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 && (row[i] >= 2 || column[j] >= 2) {
+				result++
+			}
+		}
+	}
+
+	return result
+}
+
+// @lc code=end
