ad999dcb5b619542b0b60f3893974b8606b7f2bb
yuhua <2651034096@qq.com>
2023-08-24 16:14:45 +0800

    513.找树左下角的值

---
+++ 513.找树左下角的值.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=golang
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) (ans int) {
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		if node.Right != nil {
+			q = append(q, node.Right)
+		}
+		if node.Left != nil {
+			q = append(q, node.Left)
+		}
+		ans = node.Val
+	}
+	return
+}
+
+// @lc code=end
