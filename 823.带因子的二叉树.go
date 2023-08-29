f4a27b1a26dd0a73d080dc72b4df080e92622ab0
yuhua <2651034096@qq.com>
2023-08-29 09:19:08 +0800

    823.带因子的二叉树

---
+++ 823.带因子的二叉树.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=823 lang=golang
+ *
+ * [823] 带因子的二叉树
+ */
+
+// @lc code=start
+func numFactoredBinaryTrees(arr []int) int {
+	sort.Ints(arr)
+	result, mod := int(0), int(1e9+7)
+	dp := make([]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		dp[i] = 1
+		for left, right := 0, i-1; left <= right; left++ {
+			for left <= right && arr[left]*arr[right] > arr[i] {
+				right--
+			}
+			if left <= right && arr[left]*arr[right] == arr[i] {
+				if left == right {
+					dp[i] = (dp[i] + dp[left]*dp[right]) % mod
+				} else {
+					dp[i] = (dp[i] + dp[left]*dp[right]*2) % mod
+				}
+			}
+		}
+		result = (result + dp[i]) % mod
+	}
+	return result
+}
+
+// @lc code=end
