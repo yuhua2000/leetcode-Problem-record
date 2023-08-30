e3237f15bd7e98a45b11f0f6a83fb37d674bc05d
yuhua <2651034096@qq.com>
2023-08-30 10:47:42 +0800

    1654.到家的最少跳跃次数

---
+++ 1654.到家的最少跳跃次数.go
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=1654 lang=golang
+ *
+ * [1654] 到家的最少跳跃次数
+ */
+
+// @lc code=start
+func minimumJumps(forbidden []int, a int, b int, x int) int {
+	lower := 0
+	maxVal := 0
+	for _, val := range forbidden {
+		maxVal = max(maxVal, val)
+	}
+	upper := max(maxVal+a, x) + b
+	q := [][3]int{[3]int{0, 1, 0}}
+	visited := make(map[int]bool)
+	forbiddenSet := make(map[int]bool)
+	visited[0] = true
+
+	for _, position := range forbidden {
+		forbiddenSet[position] = true
+	}
+	for len(q) > 0 {
+		position, direction, step := q[0][0], q[0][1], q[0][2]
+		q = q[1:]
+		if position == x {
+			return step
+		}
+		nextPosition, nextDirection := position+a, 1
+		_, ok1 := visited[nextPosition*nextDirection]
+		_, ok2 := forbiddenSet[nextPosition]
+		if lower <= nextPosition && nextPosition <= upper && !ok1 && !ok2 {
+			visited[nextPosition*nextDirection] = true
+			q = append(q, [3]int{nextPosition, nextDirection, step + 1})
+		}
+		if direction == 1 {
+			nextPosition, nextDirection := position-b, -1
+			_, ok1 := visited[nextPosition*nextDirection]
+			_, ok2 := forbiddenSet[nextPosition]
+			if lower <= nextPosition && nextPosition <= upper && !ok1 && !ok2 {
+				visited[nextPosition*nextDirection] = true
+				q = append(q, [3]int{nextPosition, nextDirection, step + 1})
+			}
+		}
+	}
+	return -1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
