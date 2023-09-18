5293d0c5f76f11b6dc061637537da911d384741e
yuhua <2651034096@qq.com>
2023-09-18 09:15:19 +0800

    LCP_50.宝石补给

---
+++ LCP_50. 宝石补给.go
@@ -0,0 +1,17 @@
+func giveGem(gem []int, operations [][]int) int {
+	for _, operations := range operations {
+		temp := gem[operations[0]] / 2
+		gem[operations[1]] += temp
+		gem[operations[0]] -= temp
+	}
+	max, min := gem[0], gem[1]
+	for _, g := range gem {
+		if g > max {
+			max = g
+		}
+		if g < min {
+			min = g
+		}
+	}
+	return max - min
+}
