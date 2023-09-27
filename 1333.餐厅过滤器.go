fb981e12ef21a898a338b9832389921ce1a55462
yuhua <2651034096@qq.com>
2023-09-27 09:23:24 +0800

    1333.餐厅过滤器

---
+++ 1333.餐厅过滤器.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1333 lang=golang
+ *
+ * [1333] 餐厅过滤器
+ */
+
+// @lc code=start
+func filterRestaurants(restaurants [][]int, veganFriendly int, maxPrice int, maxDistance int) []int {
+	eligibleRestaurants := make([][]int, 0)
+	for i := 0; i < len(restaurants); i++ {
+		if restaurants[i][2] >= veganFriendly && restaurants[i][3] <= maxPrice && restaurants[i][4] <= maxDistance {
+			eligibleRestaurants = append(eligibleRestaurants, restaurants[i])
+		}
+	}
+	sort.Slice(eligibleRestaurants, func(i, j int) bool {
+		if eligibleRestaurants[i][1] == eligibleRestaurants[j][1] {
+			return eligibleRestaurants[i][0] > eligibleRestaurants[j][0]
+		} else {
+			return eligibleRestaurants[i][1] > eligibleRestaurants[j][1]
+		}
+	})
+	result := make([]int, len(eligibleRestaurants))
+	for i := 0; i < len(eligibleRestaurants); i++ {
+		result[i] = eligibleRestaurants[i][0]
+	}
+	return result
+}
+
+// @lc code=end
--- 2582.递枕头.go
+++ 2582.递枕头.go
@@ -1,5 +1,3 @@
-package leetcode
-
 func passThePillow(n int, time int) int {
 	time %= (n - 1) * 2
 	if time <= n {
