4530681275b323fd1a865d4725b5a8ffef851d63
yuhua <2651034096@qq.com>
2023-10-26 09:09:52 +0800

    2520.统计能整除数字的位数

---
+++ 2520.统计能整除数字的位数.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2520 lang=golang
+ *
+ * [2520] 统计能整除数字的位数
+ */
+
+// @lc code=start
+func countDigits(num int) int {
+	result := 0
+	for i := num; i > 0; i /= 10 {
+		if num%(i%10) == 0 {
+			result++
+		}
+	}
+	return result
+}
+
+// @lc code=end
