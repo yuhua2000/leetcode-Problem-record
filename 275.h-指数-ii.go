47d7f3bb3d9f5773f6a59da2db014c4ef0c4c972
yuhua <2651034096@qq.com>
2023-10-30 09:29:13 +0800

    275.h-指数-ii

---
+++ 275.h-指数-ii.go
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=275 lang=golang
+ *
+ * [275] H 指数 II
+ */
+
+// @lc code=start
+func hIndex(citations []int) int {
+	n := len(citations)
+	return n - sort.Search(n, func(x int) bool { return citations[x] >= n-x })
+}
+
+// @lc code=end
