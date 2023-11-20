0a3b5a3567ab6fc152ccfbea18cc1c299e75b9b4
yuhua <2651034096@qq.com>
2023-11-20 17:45:21 +0800

    117.填充每个节点的下一个右侧节点指针 II

--- 117.填充每个节点的下一个右侧节点指针-ii.go
+++ 117.填充每个节点的下一个右侧节点指针-ii.go
@@ -15,7 +15,7 @@
  * }
  */
 
-func connect(root *Node) *Node {
+func connect1(root *Node) *Node {
 	if root == nil {
 		return nil
 	}
@@ -34,6 +34,31 @@ func connect(root *Node) *Node {
 				queue = append(queue, node.Right)
 			}
 		}
+	}
+	return root
+}
+
+func connect(root *Node) *Node {
+	start := root
+	for start != nil {
+		var nextStart, last *Node
+		handle := func(cur *Node) {
+			if cur == nil {
+				return
+			}
+			if nextStart == nil {
+				nextStart = cur
+			}
+			if last != nil {
+				last.Next = cur
+			}
+			last = cur
+		}
+		for p := start; p != nil; p = p.Next {
+			handle(p.Left)
+			handle(p.Right)
+		}
+		start = nextStart
 	}
 	return root
 }
