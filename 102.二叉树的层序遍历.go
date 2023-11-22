f866fd9632f245f759b622d27834a9ea4ee0119a
yuhua <2651034096@qq.com>
2023-11-22 09:13:07 +0800

    102.二叉树的层序遍历

---
+++ 102.二叉树的层序遍历.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=golang
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	result := make([][]int, 0)
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		tmp := queue
+		queue = nil
+		layer := make([]int, 0, len(tmp))
+		for _, node := range tmp {
+			layer = append(layer, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		result = append(result, layer)
+	}
+	return result
+}
+
+// @lc code=end
