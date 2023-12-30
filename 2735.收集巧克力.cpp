d5e72f8a67acbfc82fe85e2f2d4040a13a7f585a
yuhua <2651034096@qq.com>
2023-12-30 17:16:46 +0800

    2735.收集巧克力

---
+++ 2735.收集巧克力.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+    public:
+    long long minCost(vector<int>& nums, int x) {
+        int n = nums.size();
+        vector<int> f(nums);
+        long long ans = accumulate(f.begin(), f.end(), 0LL);
+        for (int k = 0; k < n; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f[i] = min(f[i], nums[(i + k) % n]);
+            }
+            ans = min(ans, static_cast<long long>(k) * x + accumulate(f.begin(), f.end(), 0LL));
+        }
+        return ans;
+    }
+};
