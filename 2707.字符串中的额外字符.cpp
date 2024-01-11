f99bbddcaa522e512c2972765751162ddd500575
yuhua <2651034096@qq.com>
2024-01-11 20:55:42 +0800

    2645.构造有效字符串的最少插入数

---
+++ 2707.字符串中的额外字符.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Trie {
+	private:
+	vector<Trie*> children;
+	bool isEnd;
+
+	public:
+	Trie();children(16), isEnd(false);
+
+	void insert(string word)
+	{
+		
+	}
+}
+
+class Solution {
+	public:
+	int minExtraChar(string s, vector<string>& dictionary)
+	{
+
+	}
+};
