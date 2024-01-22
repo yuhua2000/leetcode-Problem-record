7e668d952478a6883bdbfeebd3e35c467cb012ff
yuhua <2651034096@qq.com>
2024-01-22 20:39:33 +0800

    1944.队列中可以看到的人数.cpp

--- 1944.队列中可以看到的人数.cpp
+++ 1944.队列中可以看到的人数.cpp
@@ -12,8 +12,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
-        
-        
+        int n = heights.size();
+        vector<int> stack;
+        vector<int> res(n, 0);
+        for (int i = n-1; i >=0; i--)
+        {
+            int h = heights[i];
+            while (!stack.empty()&&stack.back()<h)
+            {
+                stack.pop_back();
+                res[i]++;
+            }
+            if (!stack.empty())
+            {
+                res[i]++;
+            }
+            stack.push_back(h);
+        }
+
+        return res;
     }
 };
 // @lc code=end
