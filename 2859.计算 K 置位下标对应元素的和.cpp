bd2f5a277d536b9f45be366081e7a730e6137ce2
yuhua <2651034096@qq.com>
2024-01-25 09:54:41 +0800

    2859.计算 K 置位下标对应元素的和

---
+++ 2859.计算 K 置位下标对应元素的和.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+    public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = i;
+            int bits = 0;
+            while (num > 0)
+            {
+                bits++;
+                num &= (num - 1);
+            }
+            if (bits == k)
+            {
+                result += nums[i];
+            }
+        }
+        return result;
+    }
+};
