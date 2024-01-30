ed4ac9d5e702571f565de1bb1a2aa9bc035ac87d
yuhua <2651034096@qq.com>
2024-01-30 11:50:57 +0800

    2808.使循环数组所有元素相等的最少秒数

---
+++ 2808.使循环数组所有元素相等的最少秒数.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+    int minimumSeconds(vector<int>& nums)
+    {
+        unordered_map<int, vector<int>> mp;
+        int n = nums.size(), result = n;
+        for (int i = 0; i < n; i++)
+        {
+            mp[nums[i]].push_back(i);
+        }
+
+        for (auto& pos : mp)
+        {
+            int mx = pos.second[0] + n - pos.second.back();
+            for (int i = 1; i < pos.second.size(); i++)
+            {
+                mx = max(mx, pos.second[i] - pos.second[i - 1]);
+            }
+            result = min(result, mx / 2);
+        }
+
+        return result;
+    }
+};
+ 
