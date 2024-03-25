c54b1bab472bbd40c96d98be706a181313784fec
yuhua <2651034096@qq.com>
2024-03-25 19:38:45 +0800

    518.零钱兑换-ii

---
+++ 518.零钱兑换-ii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+#include <vector>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+    public:
+    int change(int amount, vector<int>& coins)
+    {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (auto coin : coins)
+        {
+            for (int i = coin; i <= amount; i++)
+            {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+
+/*
+
+*/
+// @lc code=end
+
