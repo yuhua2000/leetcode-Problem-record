02401abd3bbf7f903f4e869b3d7a21f237beccb9
yuhua <2651034096@qq.com>
2024-05-15 17:17:49 +0800

    2589.完成所有任务的最少时间

---
+++ 2589.完成所有任务的最少时间.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+    public:
+    int findMinimumTime(vector<vector<int>>& tasks)
+    {
+        size_t n = tasks.size();
+        sort(tasks.begin(), tasks.end(), [&] (const vector<int>& a, const  vector<int>& b)->bool {
+            return a[1] < b[1];
+            }
+        );
+        vector<int> run(tasks[n - 1][1] + 1);
+        int res = 0;
+        for (size_t i = 0; i < n; i++)
+        {
+            int start = tasks[i][0], end = tasks[i][1], duration = tasks[i][2];
+            duration -= accumulate(run.begin() + start, run.begin() + end + 1, 0);
+            res += max(duration, 0);
+            if (duration > 0)
+            {
+                for (int j = end; j > 0 && duration > 0; j--)
+                {
+                    if (run[j] == 0)
+                    {
+                        duration--;
+                        run[j] = 1;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+
+/*
+2589. 完成所有任务的最少时间
+提示
+你有一台电脑，它可以 同时 运行无数个任务。给你一个二维整数数组 tasks ，其中 tasks[i] = [starti, endi, durationi] 表示第 i 个任务需要在 闭区间 时间段 [starti, endi] 内运行 durationi 个整数时间点（但不需要连续）。
+
+当电脑需要运行任务时，你可以打开电脑，如果空闲时，你可以将电脑关闭。
+
+请你返回完成所有任务的情况下，电脑最少需要运行多少秒。
+
+
+
+示例 1：
+
+输入：tasks = [[2,3,1],[4,5,1],[1,5,2]]
+输出：2
+解释：
+- 第一个任务在闭区间 [2, 2] 运行。
+- 第二个任务在闭区间 [5, 5] 运行。
+- 第三个任务在闭区间 [2, 2] 和 [5, 5] 运行。
+电脑总共运行 2 个整数时间点。
+示例 2：
+
+输入：tasks = [[1,3,2],[2,5,3],[5,6,2]]
+输出：4
+解释：
+- 第一个任务在闭区间 [2, 3] 运行
+- 第二个任务在闭区间 [2, 3] 和 [5, 5] 运行。
+- 第三个任务在闭区间 [5, 6] 运行。
+电脑总共运行 4 个整数时间点。
+
+
+提示：
+
+1 <= tasks.length <= 2000
+tasks[i].length == 3
+1 <= starti, endi <= 2000
+1 <= durationi <= endi - starti + 1
+*/
