211866e338c07e5b0144e176cffec85f5a861481
yuhua <2651034096@qq.com>
2024-11-07 09:20:30 +0800

    3254.长度为 K 的子数组的能量值 I.go

---
+++ 3254.长度为 K 的子数组的能量值 I.go
@@ -0,0 +1,18 @@
+func resultsArray(nums []int, k int) []int {
+	n := len(nums)
+	cnt := 0
+	ans := make([]int, n-k+1)
+	for i := 0; i < len(nums); i++ {
+		if i == 0 || nums[i]-nums[i-1] != 1 {
+			cnt = 1
+		} else {
+			cnt++
+		}
+		if cnt > k {
+			ans[i-k+1] = nums[i]
+		} else if i-k+1 >= 0 {
+			ans[i-k+1] = -1
+		}
+	}
+	return ans
+}
