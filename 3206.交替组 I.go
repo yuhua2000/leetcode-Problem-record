26b97dc434a53d41aa1ec19554909ed370ce010f
yuhua <2651034096@qq.com>
2024-12-03 20:12:54 +0800

    3206.交替组 I.go

---
+++ 3206.交替组 I.go
@@ -0,0 +1,11 @@
+func numberOfAlternatingGroups(colors []int) (result int) {
+	for i := 0; i < len(colors); i++ {
+		start := ((i - 1) + len(colors)) % len(colors)
+		end := (i + 1) % len(colors)
+
+		if colors[i] != colors[start] && colors[i] != colors[end] {
+			result++
+		}
+	}
+	return result
+}
