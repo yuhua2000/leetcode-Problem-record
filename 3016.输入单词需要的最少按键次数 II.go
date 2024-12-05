8c786a1e014a3f0b727ef2d61baa5ca9c2ac245e
yuhua <2651034096@qq.com>
2024-12-05 20:33:21 +0800

    3016.输入单词需要的最少按键次数 II.go

---
+++ 3016.输入单词需要的最少按键次数 II.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import (
+	"maps"
+	"sort"
+)
+
+func minimumPushes(word string) int {
+	ans := make(map[byte]int)
+	for i := range word {
+		ans[word[i]]++
+	}
+	maps.DeleteFunc(ans, func(k byte, v int) bool { return v == 0 })
+
+	letters := make([]int, 0, len(ans))
+	for k := range maps.Values(ans) {
+		letters = append(letters, k)
+	}
+	sort.Slice(letters, func(i, j int) bool {
+		return letters[i] > letters[j]
+	})
+	reuslt := 0
+	for i, k := range letters {
+		reuslt += ((i + 8) / 8) * k
+	}
+	return reuslt
+}
