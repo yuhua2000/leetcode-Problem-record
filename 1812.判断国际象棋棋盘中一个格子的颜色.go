985a586616eb62405f3d81a92ee89cb23a85a118
yuhua <2651034096@qq.com>
2024-12-09 09:37:37 +0800

    1812.判断国际象棋棋盘中一个格子的颜色.go

---
+++ 1812.判断国际象棋棋盘中一个格子的颜色.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=1812 lang=golang
+ *
+ * [1812] 判断国际象棋棋盘中一个格子的颜色
+ */
+
+// @lc code=start
+func squareIsWhite(coordinates string) bool {
+	return (coordinates[0]-'a'+coordinates[1]-'1')%2 == 1
+}
+
+// @lc code=end
