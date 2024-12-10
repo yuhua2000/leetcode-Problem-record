c61e5dd9c1e69df15f738a3eb8536186e6ffab66
yuhua <2651034096@qq.com>
2024-12-10 20:17:56 +0800

    935.骑士拨号器.go

---
+++ 935.骑士拨号器.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=935 lang=golang
+ *
+ * [935] 骑士拨号器
+ */
+
+// @lc code=start
+func knightDialer(n int) int {
+	const mod = 1_000_000_007
+	moves := [][]int{
+		{4, 6},
+		{6, 8},
+		{7, 9},
+		{4, 8},
+		{3, 9, 0},
+		{},
+		{1, 7, 0},
+		{2, 6},
+		{1, 3},
+		{2, 4},
+	}
+	d := [2][10]int{}
+	for i := 0; i < 10; i++ {
+		d[1][i] = 1
+	}
+	for i := 2; i <= n; i++ {
+		x := i & 1
+		for j := 0; j < 10; j++ {
+			d[x][j] = 0
+			for _, k := range moves[j] {
+				d[x][j] = (d[x][j] + d[x^1][k]) % mod
+			}
+		}
+	}
+	res := 0
+	for _, x := range d[n%2] {
+		res = (res + x) % mod
+	}
+	return res
+
+}
+
+// @lc code=end
