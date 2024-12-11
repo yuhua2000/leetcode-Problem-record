417f5f59d06b92ad9ac45c99ca3f0889ccbfa5f0
yuhua <2651034096@qq.com>
2024-12-11 20:08:47 +0800

    2717.半有序排列.go

---
+++ 2717.半有序排列.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func semiOrderedPermutation(nums []int) int {
+	first, last := 0, 0
+	n := len(nums)
+	for i, num := range nums {
+		if num == 1 {
+			first = i
+		}
+		if num == n {
+			last = i
+		}
+	}
+
+	if first > last {
+		return first + n - 1 - last - 1
+	}
+	return first + n - 1 - last
+}
