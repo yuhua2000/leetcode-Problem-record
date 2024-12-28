fe34e600848889b2317434aaa4cc7ea5c2b3bea5
yuhua <2651034096@qq.com>
2024-12-28 09:21:05 +0800

    3046.分割数组

---
+++ 3046.分割数组.go
@@ -0,0 +1,10 @@
+func isPossibleToSplit(nums []int) bool {
+	numFlags := [101]int{}
+	for _, num := range nums {
+		numFlags[num]++
+		if numFlags[num] == 3 {
+			return false
+		}
+	}
+	return true
+}
