20900db04b5746868e86a20d83817f7fbd0b63c8
yuhua <2651034096@qq.com>
2024-12-31 09:49:15 +0800

    2181.合并零之间的节点

---
+++ 2181.合并零之间的节点.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=2181 lang=golang
+ *
+ * [2181] 合并零之间的节点
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeNodes(head *ListNode) *ListNode {
+	h := &ListNode{Next: head}
+	next := h.Next
+	for next != nil {
+		next = next.Next
+		v := 0
+		for next != nil && next.Val != 0 {
+			v += next.Val
+			next = next.Next
+		}
+		if v == 0 {
+			h.Next = nil
+			break
+		}
+		h = h.Next
+		h.Val = v
+
+	}
+
+	return head
+}
+
+// @lc code=end
