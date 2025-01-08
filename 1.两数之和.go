117eb2501106ac88e76d1fa05dc9b45007240f69
yuhua <2651034096@qq.com>
2025-01-08 20:40:22 +0800

    1.两数之和

---
+++ 1.两数之和.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	prevNum := make(map[int]int)
+	for i, num := range nums {
+		if p, ok := prevNum[target-num]; ok {
+			return []int{i, p}
+		}
+		prevNum[num] = i
+	}
+	return nil
+}
+
+// @lc code=end
