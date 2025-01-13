646acd79e97fcc208d94379c5dd1bac3a24a9bcf
yuhua <2651034096@qq.com>
2025-01-13 17:04:30 +0800

    2270.分割数组的方案数

---
+++ 2270.分割数组的方案数.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2270 lang=golang
+ *
+ * [2270] 分割数组的方案数
+ */
+
+// @lc code=start
+func waysToSplitArray(nums []int) int {
+	var l, r int64
+	for _, num := range nums {
+		r += int64(num)
+	}
+
+	var result int
+	for i := 0; i < len(nums)-1; i++ {
+		l += int64(nums[i])
+		r -= int64(nums[i])
+		if l >= r {
+			result++
+		}
+	}
+	return result
+}
+
+// @lc code=end
