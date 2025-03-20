8f2db75c167e2858485a71a3588998a8d227d930
yuhua <2651034096@qq.com>
2025-03-20 20:45:50 +0800

    2610.转换二维数组.go

---
+++ 2610.转换二维数组.go
@@ -0,0 +1,21 @@
+func findMatrix(nums []int) [][]int {
+	slices.Sort(nums)
+ 
+	result := [][]int{[]int{nums[0]}}
+	level := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			level++
+		} else {
+			level = 0
+		}
+
+		if level < len(result) {
+			result[level] = append(result[level], nums[i])
+		} else {
+			result = append(result, []int{nums[i]})
+		}
+	}
+
+	return result
+}
