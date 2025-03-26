4bbdae9d53344383618d40f970bf7010d806a916
yuhua <2651034096@qq.com>
2025-03-26 09:43:16 +0800

    2829.k-avoiding 数组的最小总和

---
+++ 2829.k-avoiding 数组的最小总和.go
@@ -0,0 +1,16 @@
+func minimumSum(n int, k int) int {
+	result := 0
+	for i := 1; i <= k/2 && i <= n; i++ {
+		result += i
+	}
+
+	ans := k
+	if ans == (k)/2 {
+		ans++
+	}
+	for i := 0; i < n-k/2; i++ {
+		result += ans
+		ans++
+	}
+	return result
+}
