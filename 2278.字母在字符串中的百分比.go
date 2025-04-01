85c4e7b02b673334115829977d2be63560a0c323
yuhua <2651034096@qq.com>
2025-04-01 20:36:51 +0800

    2278.字母在字符串中的百分比

---
+++ 2278.字母在字符串中的百分比.go
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=2278 lang=golang
+ *
+ * [2278] 字母在字符串中的百分比
+ */
+
+// @lc code=start
+func percentageLetter(s string, letter byte) int {
+	var numerator int
+	for i := 0; i < len(s); i++ {
+		if s[i] == letter {
+			numerator++
+		}
+	}
+
+	return 100 * numerator / len(s)
+}
+
+// @lc code=end
