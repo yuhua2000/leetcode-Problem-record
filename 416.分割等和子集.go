7d1df7fbd6c17ae136062d4ed2859660920ec1fc
yuhua <2651034096@qq.com>
2025-04-08 19:54:26 +0800

    416.分割等和子集

---
+++ 416.分割等和子集.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=golang
+ *
+ * [416] 分割等和子集
+ */
+
+// @lc code=start
+func canPartition(nums []int) bool {
+	n := len(nums)
+	if n < 2 {
+		return false
+	}
+
+	sum := 0
+	maxNum := 0
+	for _, num := range nums {
+		sum += num
+		maxNum = max(maxNum, num)
+	}
+
+	target := sum / 2
+	if sum%2 == 1 || maxNum > target {
+		return false
+	}
+
+	dp := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]bool, target+1)
+		dp[i][0] = true
+	}
+	dp[0][nums[0]] = true
+
+	for i := 1; i < n; i++ {
+		v := nums[i]
+		for j := 1; j <= target; j++ {
+			if j >= v {
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-v]
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[n-1][target]
+
+}
+
+// @lc code=end
