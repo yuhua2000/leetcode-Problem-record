38ee4263cdaf72642cb3d01ac693e5b87d5f508e
yuhua <2651034096@qq.com>
2025-09-16 17:28:18 +0800

    743.网络延迟时间 Dijkstra 算法

--- 743.网络延迟时间.go
+++ 743.网络延迟时间.go
@@ -1,6 +1,7 @@
 package leetcode
 
 import (
+	"container/heap"
 	"math"
 )
 
@@ -11,7 +12,8 @@ import (
  */
 
 // @lc code=start
-func networkDelayTime(times [][]int, n int, k int) int {
+
+func networkDelayTime1(times [][]int, n int, k int) int {
 	dp := make([]int, n+1)
 	for i := range dp {
 		dp[i] = math.MaxInt
@@ -36,6 +38,65 @@ func networkDelayTime(times [][]int, n int, k int) int {
 	}
 
 	return maxTiem
+}
+
+type Item struct {
+	node int
+	dist int
+}
+
+type MinHeap []Item
+
+func (m MinHeap) Len() int           { return len(m) }
+func (m MinHeap) Less(i, j int) bool { return m[i].dist < m[j].dist }
+func (m MinHeap) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+
+func (m *MinHeap) Push(item any) {
+	*m = append(*m, item.(Item))
+}
+
+func (m *MinHeap) Pop() any {
+	x := (*m)[len(*m)-1]
+	*m = (*m)[:len(*m)-1]
+	return x
+}
+
+func networkDelayTime(times [][]int, n int, k int) int {
+	graph := make([][][2]int, n+1)
+	for _, time := range times {
+		graph[time[0]] = append(graph[time[0]], [2]int{time[1], time[2]})
+	}
+
+	dist := make([]int, n+1)
+	for i := range dist {
+		dist[i] = math.MaxInt32
+	}
+
+	dist[k] = 0
+
+	h := &MinHeap{}
+	heap.Push(h, Item{node: k, dist: 0})
+
+	for h.Len() > 0 {
+		itme := heap.Pop(h).(Item)
+		for _, neighbor := range graph[itme.node] {
+			v, w := neighbor[0], neighbor[1]
+			if dist[itme.node]+w < dist[v] {
+				dist[v] = dist[itme.node] + w
+				heap.Push(h, Item{node: v, dist: dist[v]})
+			}
+		}
+	}
+
+	maxTime := 0
+	for i := 1; i <= n; i++ {
+		if dist[i] == math.MaxInt32 {
+			return -1
+		}
+		maxTime = max(maxTime, dist[i])
+	}
+
+	return maxTime
 }
 
 // @lc code=end
