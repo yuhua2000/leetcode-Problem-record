f214160252611cb101938a51cb2aaab17ff48c7d
yuhua <2651034096@qq.com>
2025-09-23 19:41:49 +0800

    165.比较版本号

--- 165.比较版本号.go
+++ 165.比较版本号.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"strconv"
+	"strings"
 )
 
 /*
@@ -12,36 +13,8 @@ import (
 
 // @lc code=start
 func compareVersion(version1 string, version2 string) int {
-	fmtVersion := func(version string) (result []string) {
-		b := true
-		var node []byte
-		for i := 0; i < len(version); i++ {
-			switch version[i] {
-			case '.':
-				if len(node) > 0 {
-					result = append(result, string(node))
-				} else {
-					result = append(result, "0")
-				}
-				node = node[:0]
-				b = true
-			case '0':
-				if !b {
-					node = append(node, version[i])
-				}
-			default:
-				b = false
-				node = append(node, version[i])
-			}
-		}
-		if len(node) > 0 {
-			result = append(result, string(node))
-		}
-		return
-	}
-
-	fmtVer1 := fmtVersion(version1)
-	fmtVer2 := fmtVersion(version2)
+	fmtVer1 := strings.Split(version1, ".")
+	fmtVer2 := strings.Split(version2, ".")
 	for i := 0; i < max(len(fmtVer1), len(fmtVer2)); i++ {
 		var ver1, ver2 int
 		if i < len(fmtVer1) {
